bc3d724a3991b23692dd4e8651775f36428bbb35
Yuvaraj Poduri <118831011+yuvarajpoduri@users.noreply.github.com>
2025-06-28 00:06:01 +0530

    Update events.js

--- events.js
+++ events.js
@@ -6,7 +6,7 @@ const announcementsData = {
       content:
         "Hey fam! 💖 Today we’re showering all our love and warmest wishes on our amazing Poduri Mounika, who turns 26 today! 🎈🎁 May this new year bring you endless joy, success, and beautiful memories. Let’s make her day extra special—drop your wishes and let her feel the whole family’s love from every corner. ❤️🥳",
       image:
-        "https://cdn.pixabay.com/photo/2020/10/23/07/02/happy-birthday-5671349_1280.jpg",
+        "https://images.app.goo.gl/eNSpQ",
       date: "2025-06-28T08:00:00Z",
       timestamp: "2025-06-28T08:00:00Z",
     },
