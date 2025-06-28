2b2a9c087a88faee752ce5c51bcb0f06421746fd
Yuvaraj Poduri <118831011+yuvarajpoduri@users.noreply.github.com>
2025-06-28 14:04:17 +0530

    Update events.js

--- events.js
+++ events.js
@@ -10,6 +10,27 @@ const announcementsData = {
       date: "2025-06-28T14:30:00Z",
       timestamp: "2025-06-28T14:30:00Z",
     },
+     
+    {
+      id: 2,
+      heading: "👨‍👩‍👧‍👦 Family Member Profiles Updated!",
+      content:
+        "Exciting update! 🎉 You can now view detailed profiles of all our beloved family members—complete with their DPs! 📸👤 It's never been easier to put a face to a name. [Explore the family](https://yourfamilydetailsite.com) and celebrate our awesome tribe! 🫶",
+      image:
+        "https://images.squarespace-cdn.com/content/v1/5ec689480cc22c2d441e152f/1596827958143-DFUFPS9P6JXQ5L4UGE52/family-photography-gallery-wall-greenwich-ct-photographer-entry.jpeg",
+      date: "2025-06-28T15:00:00Z",
+      timestamp: "2025-06-28T15:00:00Z",
+    },
+    {
+      id: 3,
+      heading: "✨ Chat Feature Upgraded! ✨",
+      content:
+        "We’ve given our family chat a fresh new look! 😎🎨 From sleek styling to exciting reactions and replies, everything’s smoother and more fun than ever before! 🗨️💬 Don’t miss out—[Chat now](https://yourchatappsite.com) and see what everyone’s talking about! 💖",
+      image:
+        "https://media.istockphoto.com/id/1306478973/vector/computer-online-chat-notices.jpg?s=612x612&w=0&k=20&c=D7anWrKC0Y8072mw5_AERXaeq2D4MLbjh4g6hu7Vla4=",
+      date: "2025-06-28T15:30:00Z",
+      timestamp: "2025-06-28T15:30:00Z",
+    },
   ],
 };
 
