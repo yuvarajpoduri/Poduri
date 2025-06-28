1b79cef8645957f75a556017219ab98330b4e04a
Yuvaraj Poduri <118831011+yuvarajpoduri@users.noreply.github.com>
2025-06-28 14:57:32 +0530

    Update events.js

--- events.js
+++ events.js
@@ -7,8 +7,8 @@ const announcementsData = {
         "Hey fam! 💖 Today we’re showering all our love and warmest wishes on our amazing Poduri Mounika, who turns 26 today! 🎈🎁 May this new year bring you endless joy, success, and beautiful memories. Let’s make her day extra special—drop your wishes and let her feel the whole family’s love from every corner. ❤️🥳",
       image:
         "https://marketplace.canva.com/EAE85sEiCbU/1/0/1600w/canva-pastel-birthday-birthday-facebook-cover-llYsuXgP0Mc.jpg",
-      date: "2025-06-28T14:30:00Z",
-      timestamp: "2025-06-28T14:30:00Z",
+      date: "2025-06-28T09:30:00Z",
+      timestamp: "2025-06-28T09:30:00Z",
     },
      
     {
@@ -18,8 +18,8 @@ const announcementsData = {
         "Exciting update! 🎉 You can now view detailed profiles of all our beloved family members—complete with their DPs! 📸👤 It's never been easier to put a face to a name. and celebrate our awesome tribe! 🫶",
       image:
         "https://images.squarespace-cdn.com/content/v1/5ec689480cc22c2d441e152f/1596827958143-DFUFPS9P6JXQ5L4UGE52/family-photography-gallery-wall-greenwich-ct-photographer-entry.jpeg",
-      date: "2025-06-28T15:00:00Z",
-      timestamp: "2025-06-28T15:00:00Z",
+      date: "2025-06-28T10:00:00Z",
+      timestamp: "2025-06-28T10:00:00Z",
     },
     {
       id: 3,
@@ -28,8 +28,8 @@ const announcementsData = {
         "We’ve given our family chat a fresh new look! 😎🎨 From sleek styling to exciting reactions and replies, everything’s smoother and more fun than ever before! 🗨️💬 Don’t miss out and see what everyone’s talking about! 💖",
       image:
         "https://media.istockphoto.com/id/1306478973/vector/computer-online-chat-notices.jpg?s=612x612&w=0&k=20&c=D7anWrKC0Y8072mw5_AERXaeq2D4MLbjh4g6hu7Vla4=",
-      date: "2025-06-28T15:30:00Z",
-      timestamp: "2025-06-28T15:30:00Z",
+      date: "2025-06-28T10:30:00Z",
+      timestamp: "2025-06-28T10:30:00Z",
     },
   ],
 };
